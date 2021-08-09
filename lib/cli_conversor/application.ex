49ab6d0b39283c4b16ee3b9d9edc2f25a39c8e27
Rhiad Adel <rhiad.ciccoli@gmail.com>
2021-08-09 20:02:59 -0300

    create application

---
+++ lib/cli_conversor/application.ex
@@ -0,0 +1,18 @@
+defmodule CliConversor.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {CliConversor.Interaction.InteractionAgent, %CliConversor.Interaction{}}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: CliConversor.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end

0c44733e17c09f1c3fb4ff04de37168a995a8d99
Rhiad Adel <rhiad.ciccoli@gmail.com>
2021-08-09 20:04:48 -0300

    update main

---
+++ lib/cli_conversor/cli/main.ex
@@ -0,0 +1,24 @@
+defmodule CliConversor.CLI.Main do
+
+  alias Mix.Shell.IO, as: Shell
+
+  @spec start_conversor :: :ok
+  def start_conversor do
+    welcome_message()
+    Shell.prompt("Press Enter to continue")
+    currency_choice()
+  end
+
+  defp welcome_message do
+    Shell.info("===============================================================")
+    Shell.info("=============~~~ Conversor de Moedas e Criptos ~~~=============")
+    Shell.info("===============================================================\n")
+    Shell.info("Você poderá fazer consultas e armazena-las em um arquivo TXT...")
+    Shell.info("Para consultar o arquivo, digite HISTORY ao sair.")
+    Shell.info("Comece escolhendo sua moeda de referência (escolha pelo número): ")
+  end
+
+  defp currency_choice do
+    CliConversor.CLI.CurrencyChoice.start()
+  end
+end

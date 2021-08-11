e31db8fe27a7cb139c540d0e5ea1282acb36c757
Rhiad Adel <rhiad.ciccoli@gmail.com>
2021-08-11 16:06:32 -0300

    update currency choice for accessing currency agent

--- lib/cli_conversor/cli/currency_choice.ex
+++ lib/cli_conversor/cli/currency_choice.ex
@@ -3,25 +3,32 @@ defmodule CliConversor.CLI.CurrencyChoice do
   alias Mix.Shell.IO, as: Shell
   import CliConversor.CLI.BaseCommands
 
-  @spec start :: none
-  def start do
-    #Shell.cmd("clear")
-    Shell.info("===================== MOEDAS ========================")
 
-    crypto = CurrencyServices.get_crypto_list
-    currency = CurrencyServices.get_currency_list
-    total_list = crypto ++ currency
+  def start do
+    Shell.cmd("clear")
+    IO.puts "===================== MOEDAS ========================"
+
+
+    total_list = CliConversor.Currency.CurrencyAgent.value
     find_currency_by_index = &Enum.at(total_list, &1)
     interaction = CliConversor.Interaction.InteractionAgent.value
 
-    total_list
+    answer =
+      total_list
     |> Enum.map(&(&1.asset_id))
-    #|> display_options
     |> generate_question
     |> Shell.prompt
-    |> parse_answer
-    |> find_currency_by_index.()
-    |> confirm_currency(interaction)
+    |> Integer.parse
+
+    case answer do
+      :error ->
+        display_invalid_option()
+        start()
+      {option, _} ->
+        find_currency_by_index.(option - 1) |> confirm_currency(interaction)
+    end
+
+
   end
 
 
@@ -33,19 +40,21 @@ defmodule CliConversor.CLI.CurrencyChoice do
 
 
   defp modify_struct(chosen_currency, %{currency_from: nil, name_currency_from: nil} = interaction) do
-    _interaction = %{
+    %{
       interaction |
       currency_from: chosen_currency.asset_id,
-      name_currency_from: chosen_currency.name
+      name_currency_from: chosen_currency.name,
+      currency_from_price: chosen_currency.price_usd
     }
     |> CliConversor.Interaction.InteractionAgent.add |> select_currency_to()
   end
 
   defp modify_struct(chosen_currency, %{currency_to: nil, name_currency_to: nil} = interaction) do
-    _interaction = %{
+    %{
       interaction |
       currency_to: chosen_currency.asset_id,
-      name_currency_to: chosen_currency.name
+      name_currency_to: chosen_currency.name,
+      currency_to_price: chosen_currency.price_usd
     }
     |> CliConversor.Interaction.InteractionAgent.add
   end
@@ -54,4 +63,6 @@ defmodule CliConversor.CLI.CurrencyChoice do
     Shell.info("Please, select the currency you want to convert to.")
     start()
   end
+
+
 end

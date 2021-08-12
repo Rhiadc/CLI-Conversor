e8ae737be68a6ab362a4e0d8d4d5b4f713c01cb3
Rhiad Adel <rhiad.ciccoli@gmail.com>
2021-08-12 18:45:20 -0300

    modify start for initialize whole app

--- lib/mix/tasks/start.ex
+++ lib/mix/tasks/start.ex
@@ -1,5 +1,8 @@
 defmodule Mix.Tasks.Start do
-  use Mix.Task
+  use Mix.Task.Compiler
 
-  def run(_), do: CliConversor.CLI.Main.start_conversor
+  def run(_) do
+    Mix.Task.run("app.start")
+    CliConversor.CLI.Main.start_conversor
+  end
 end
